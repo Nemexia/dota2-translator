db5c8ba4429f1e00beb411553aa4db5ecd1a11f5
Nemexia <amirhossein.rahmani55@gmail.com>
2025-02-14 14:34:41 +0330

    Move screenshot code to screen.py

--- d2t/main.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-import cv2
-import numpy as np
-
-screenshot = pyautogui.screenshot()
-image = np.array(screenshot)  # Convert to NumPy array
-image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR for OpenCV
-
-cv2.imwrite("screenshot.png", image)
