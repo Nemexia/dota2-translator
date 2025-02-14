a106a834017a317e71a75518f65f6aecf5251478
Nemexia <amirhossein.rahmani55@gmail.com>
2025-02-14 18:15:35 +0330

    Refactor screen.py

--- d2t/screen.py
+++ d2t/screen.py
@@ -1,19 +1,16 @@
 import pyautogui
-import cv2
-import numpy as np
 import keyboard
 
-# Define crop area (left, top, right, bottom)
-crop_box = (550, 600, 1400, 800)  # Change these values as needed
+crop_box = (550, 600, 1400, 800)
+
 
 def take_screenshot():
-    screenshot = pyautogui.screenshot()  # Take a screenshot
-    cropped_image = screenshot.crop(crop_box)  # Crop the screenshot
-    cropped_image.save("cropped_screenshot.png")  # Save the image
+    screenshot = pyautogui.screenshot()
+    cropped_image = screenshot.crop(crop_box)
+    cropped_image.save("cropped_screenshot.png")
     print("Screenshot saved as cropped_screenshot.png")
 
-# Set hotkey (e.g., Ctrl + Shift + S)
-keyboard.add_hotkey("ctrl+shift+s", take_screenshot)
 
+keyboard.add_hotkey("ctrl+shift+s", take_screenshot)
 print("Press Ctrl + Shift + S to take a cropped screenshot...")
-keyboard.wait("esc")  # Keep script running until you press 'Esc'
+keyboard.wait("esc")
